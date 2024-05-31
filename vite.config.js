321cfc066093accae6b751fc236d9a01e31f94bb
Eze <Riccaesteban@gmail.com>
2024-05-30 23:15:19 -0300

    second commit

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "https://Eze-Ricca.github.io/todo-list", // Reemplaza <TU_USUARIO> y <REPO> con tu información específica
+  // Otras opciones de configuración...
+});
